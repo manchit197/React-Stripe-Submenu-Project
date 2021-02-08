32788d156a971cd72fde022e2cbf672df8d89d26
Jeff Suen <manchit197@gmail.com>
2021-02-08 16:12:45 +0800

    initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./Components/App";
+import { AppProvider } from "./Components/context";
+ReactDOM.render(
+  <React.StrictMode>
+    <AppProvider>
+      <App />
+    </AppProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
